e5b1132c069849d61fb29df64db667d2898dfb4c
Fiiillliiippp <filipbrady@gmail.com>
2022-11-26 22:29:06 +0100

    building a calculator using useReducer

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,86 @@
-import { useState } from 'react';
+import { useReducer } from 'react';
 import './App.css';
-import ButtonBox from './components/ButtonBox';
-import Calculator from './components/Caltulator';
-import Container from './components/Container';
-import Screen from './components/Screen';
+import CalcButton from './components/CalcButton';
+import OperationBtn from './components/OperationBtn';
+
+export const ACTION = {
+  ADD_DIGIT: 'add-digit',
+  CLEAR: 'clear',
+  DELETE_DIGIT: 'delete-digit',
+  CHOOSE_OPERATION: 'choose-operation',
+  EVALUATE: 'evaluate',
+};
+
+function reducer(state: any, { type, payload }: any) {
+  switch (type) {
+    case ACTION.ADD_DIGIT:
+      if (payload.digit === '0' && state.currentOperand === '0') {
+        return state;
+      }
+      if (payload.operation === '.' && state.currentOperand.includes('.')) {
+        return state;
+      }
+      return {
+        ...state,
+        currentOperand: `${state.currentOperand || ''}${payload.digit}`,
+      };
+      case ACTION.CLEAR:
+        return {
+          currentOperand: "",
+          previousOperand: "",
+          operation: ""
+        }
+      case ACTION.CHOOSE_OPERATION:
+        if(state.currentOperand === '0' && payload.operand === '÷') 
+          return {currentOperand: "ERROR"}
+        
+        return {
+          ...state,
+          operation: `${state.operation || ''}${payload.operation}`,
+          previousOperand: `${state.currentOperand || ''}` ,
+          currentOperand: ""
+        }
+  }
+}
 
 function App() {
-  const btnsValue = [
-    ['C', 'x2', '%', '/'],
-    [7, 8, 9, '*'],
-    [4, 5, 6, '-'],
-    [1, 2, 3, '+'],
-    ['+/-', 0, '.', '='],
-  ];
+  const [{ currentOperand, previousOperand, operation }, dispatch] = useReducer(
+    reducer,
+    {}
+  );
 
-  const [calc, setCalc] = useState({
-    sign: "",
-    num: 0,
-    res: 0
-  })
   return (
     <div className='App'>
-          <div className='CalculatorBody'>
-              <Screen value={calc.num ? calc.num : calc.res} />
-              <ButtonBox btnsValue={btnsValue} calc={calc} setCalc={setCalc} />
+      <div className='CalculatorBody'>
+        <div className='equals'>
+          <div className='previousOperand'>
+            {previousOperand} {operation}{' '}
           </div>
+          <div className='currentOperand'>{currentOperand}</div>
+        </div>
+        <div className='btnBox'>
+          <div className='btn' onClick={() => dispatch({ type: ACTION.CLEAR })} >C</div>
+          <OperationBtn dispatch={dispatch} operation='x2' />
+          <OperationBtn dispatch={dispatch} operation='÷' />
+          <div className='btn' >DEL</div>
+          <div></div><CalcButton dispatch={dispatch} digit='1' />
+          <CalcButton dispatch={dispatch} digit='2' />
+          <CalcButton dispatch={dispatch} digit='3' />
+          <OperationBtn dispatch={dispatch} operation='*' />
+          <CalcButton dispatch={dispatch} digit='4' />
+          <CalcButton dispatch={dispatch} digit='5' />
+          <CalcButton dispatch={dispatch} digit='6' />
+          <OperationBtn dispatch={dispatch} operation='+' />
+          <CalcButton dispatch={dispatch} digit='7' />
+          <CalcButton dispatch={dispatch} digit='8' />
+          <CalcButton dispatch={dispatch} digit='9' />
+          <OperationBtn dispatch={dispatch} operation='-' />
+          <OperationBtn dispatch={dispatch} operation='+/-' />
+          <CalcButton dispatch={dispatch} digit='0' />
+          <OperationBtn dispatch={dispatch} operation='.' />
+          <OperationBtn dispatch={dispatch} operation='=' />
+        </div>
+      </div>
     </div>
   );
 }
--- src/components/CalcButton.tsx
+++ src/components/CalcButton.tsx
@@ -1,121 +1,17 @@
-import { Dispatch, SetStateAction } from 'react';
+import { Dispatch } from 'react';
+import { ACTION } from '../App';
 
 type Props = {
-  value: string | number;
-  calc: { sign: string; num: number; res: number };
-  setCalc: Dispatch<SetStateAction<{ sign: string; num: number; res: number }>>;
+  dispatch: Dispatch<{ type: any; payload: any; }>;
+  digit: any
 };
-const CalcButton = ({ value, calc, setCalc }: Props) => {
-  const numClickHandler = (clickedValue: any) => {
-    const value = clickedValue;
-    console.log(value);
-    
-    if (calc.num.toLocaleString().length < 16) {
-      setCalc({
-        ...calc,
-        num:
-          calc.num === 0 && value === '0'
-            ? '0'
-            : calc.num % 1 === 0
-            ? Number(calc.num + value)
-            : calc.num + value,
-        res: !calc.sign ? 0 : calc.res,
-      });
-    }
-  };
-
-  const commaClickHandler = (clickedValue: any) => {
-    const value = clickedValue;
-
-    setCalc({
-      ...calc,
-      num: !calc.num.toString().includes('.') ? calc.num + value : calc.num,
-    });
-  };
-
-  const signClickHandler = (clickedValue: any) => {
-    const value = clickedValue;
-
-    setCalc({
-      ...calc,
-      sign: value,
-      res: !calc.res && calc.num ? calc.num : calc.res,
-      num: 0,
-    });
-  };
-
-  const equalsClickHandler = () => {
-    if (calc.sign && calc.num) {
-      const mathValue = (a: any, b: any, sign: any) =>
-        sign === '+'
-          ? a + b
-          : sign === '-'
-          ? a - b
-          : sign === '*'
-          ? a * b
-          : a / b;
-
-      setCalc({
-        ...calc,
-        sign: '',
-        num: 0,
-        res:
-          calc.num === 0 && calc.sign === '/'
-            ? "Can't divide with 0"
-            : mathValue(Number(calc.res), Number(calc.num), calc.sign),
-      });
-    }
-  };
-
-  const invertClickHandler = () => {
-    setCalc({
-      ...calc,
-      num: calc.num ? calc.num * -1 : 0,
-      res: calc.res ? calc.res * -1 : 0,
-      sign: '',
-    });
-  };
-
-  const percentClickHandler = () => {
-    let percNum = calc.num ? calc.num : 0;
-    let percRes = calc.res ? calc.res : 0;
-
-    setCalc({
-      ...calc,
-      num: (percNum /= Math.pow(100, 1)),
-      res: (percRes /= Math.pow(100, 1)),
-      sign: '',
-    });
-  };
-
-  const resetClickHandler = () => {
-    setCalc({
-      ...calc,
-      sign: '',
-      num: 0,
-      res: 0,
-    });
-  };
-
-  const handleBtnClick = () => {
-    value === 'C'
-      ? resetClickHandler()
-      : value === '+/-'
-      ? invertClickHandler()
-      : value === '%'
-      ? percentClickHandler()
-      : value === '='
-      ? equalsClickHandler()
-      : value === '/' || value === 'X' || value === '-' || value === '+'
-      ? signClickHandler(value)
-      : value === '.'
-      ? commaClickHandler(value)
-      : numClickHandler(value);
-  };
-
+const CalcButton = ({ dispatch, digit }: Props) => {
   return (
-    <div className='btn' onClick={handleBtnClick}>
-      {value}
+    <div
+      className='btn'
+      onClick={() => dispatch({ type: ACTION.ADD_DIGIT, payload: { digit } })}
+    >
+      {digit}
     </div>
   );
 };
